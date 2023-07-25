f7105fa45297820877d5052f8e1afe31049f4633
wzy <140402325+wzyovo@users.noreply.github.com>
2023-07-25 14:34:02 +0800

    Add files via upload

---
+++ 7-25.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//int main()
+//{
+////	int age = 100;
+////	if(age<18)
+////		printf("未成年\n");
+////	else if(age>=18 && age<28)
+////		printf("青年\n");
+////	else if(age>=28 && age <50)
+////		printf("壮年\n");
+////	else if(age>=50 && age<90)
+////		printf("老年\n");
+////	else
+////		printf("神仙\n");
+//    return 0;
+//}
+int main()
+{
+	int a=0;
+	int b=2;
+	if(a==1)
+		if(b==2)
+			printf("hehe\n");
+		else
+			printf("haha\n");
+	return 0;
+}
